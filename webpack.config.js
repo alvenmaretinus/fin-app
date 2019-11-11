caff94c1b4e72c384096ffc19a8991d6cc1d2fc5
Alven Maretinus <alven.maretinus@shopee.com>
2019-11-11 19:41:44 +0800

    added basic webpack config, babel config with browser polyfill and typescript support

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+    ]
+  }
+}
